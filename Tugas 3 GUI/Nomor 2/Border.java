0c396d77cb03aa4216571c0c62048c7892e37753
M. Alfan Septian Nufi <alfan.s.nufi@gmail.com>
2022-11-23 23:21:44 +0700

    Create Border.java

---
+++ Tugas 3 GUI/Nomor 2/Border.java
@@ -0,0 +1,26 @@
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class Border extends JFrame {
+    // Construct the frame 
+    public Border() {
+        super("Layout Tester");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        setLayout(new BorderLayout());
+        add(new JButton("One"),BorderLayout.NORTH);
+        add(new JButton("Two"),BorderLayout.WEST);
+        add(new JButton("Three"),BorderLayout.CENTER);
+        add(new JButton("Four"),BorderLayout.EAST);
+        add(new JButton("Five"),BorderLayout.SOUTH);
+
+        pack();
+        setVisible(true);
+    }
+
+    public static void main (String[] args) {
+        new Border();
+    }
+}
